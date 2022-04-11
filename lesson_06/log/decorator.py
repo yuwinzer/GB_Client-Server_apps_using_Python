e65b69e7026044a65720fea9984dd8dc5c2a1e66
yuwinzer <yu.moskalev@gmail.com>
2022-04-11 22:27:44 +0300

    added decorators

---
+++ lesson_06/log/decorator.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+import inspect
+
+if sys.argv[0].find('client.py') == -1:
+    LOGGER = logging.getLogger('server')
+else:
+    LOGGER = logging.getLogger('client')
+
+
+def log(func):
+    """" Функция-декоратор для логирования"""
+    def decor(*args, **kwargs):
+        result = func(*args, **kwargs)
+        s = '\\'
+        LOGGER.debug(f'Функция: "{func.__module__}/{func.__name__}({args} {kwargs})" '
+                     f'вызвана "{inspect.getmodule(inspect.stack()[1][0]).__file__.rpartition(s)[-1]}'
+                     f'/{inspect.getouterframes(inspect.currentframe())[1][3]}"; Ответ: "{str(result)}"')
+        # print(inspect.getmodule(inspect.stack()[1][0]).__file__.rpartition('\\')[-1])
+        return result
+    decor.log = LOGGER
+    return decor
